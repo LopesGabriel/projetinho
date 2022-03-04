2c65d24d75a48d49c70db3341063144010d569f9
Gabriel Lopes <lopesgabriel0199@gmail.com>
2022-03-03 21:28:00 -0300

    feat: Setup some sass configuration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,21 @@
+import { useEffect } from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { isSupported, setCurrentScreen } from 'firebase/analytics'
+import { analytics } from '../firebaseConfig'
 import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    isSupported()
+      .then((isAnalyticsSupported) => {
+        if (isAnalyticsSupported) {
+          setCurrentScreen(analytics, 'Home')
+        }
+      })
+  })
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,6 +23,10 @@ const Home: NextPage = () => {
         <meta name="description" content="Projetinho é um app para competir com seus amigos e uma forma de incentivo à atividades físicas." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <header className={styles.header}>
+        <h2>Projetinho</h2>
+      </header>
 
       <main className={styles.main}>
         <h1>Hi</h1>
